922d50e37b3158f28c0e4a922ec497d3180ea23e
Shruhrid Banthia <shrurhid.banthia@iitgn.ac.in>
2023-12-06 22:47:36 +0530

    Internal changes, resolved FastAPI error.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,30 @@
 from fastapi import FastAPI
 import pandas as pd
+import pickle
 
 app = FastAPI()
 
 # Now we load the model and label encoder
-
-import pickle
-
 model = pickle.load(open('D:\ML-5\saved\model.pkl', 'rb'))
-le = pickle.load(open('D:\ML-5\saved\label_encoder.pkl', 'rb'))
+le1 = pickle.load(open('D:\ML-5\saved\label_encoder_apmc.pkl', 'rb'))
+le2 = pickle.load(open('D:\ML-5\saved\label_encoder_commodity.pkl', 'rb'))
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/make_prediction/")
-async def read_item(commodity: str = None, APMC: str = None, date: str = None):
+async def read_item(commodity: str = None, APMC: str = None, date: str = None, arrival: int = None):
     
     # We need to convert the categorical features to numerical values
-    commodity = le.transform([commodity])
-    APMC = le.transform([APMC])
+    APMC = le1.transform([APMC])
+    commodity = le2.transform([commodity])
     
-    # We need to convert the date to datetime format
-    date = pd.to_datetime(date)
-    date = date.strftime('%Y%m').astype(int)
+    # We need to convert string date in format "YYYY-MM" to "YYYYMM"
+    date = date.replace('-', '')
 
     # We need to create a dataframe with the given values
-    df = pd.DataFrame({'Commodity': commodity, 'APMC': APMC, 'date': date})
+    df = pd.DataFrame({'APMC': APMC,'Commodity': commodity,'arrival_in_qtl': arrival, 'date': date})
 
     # We need to predict the modal price
     modal_price = model.predict(df)
--- src/model.py
+++ src/model.py
@@ -1,73 +1,55 @@
-import pandas as pd
-
-## Load cleaned data
-
-# Load data
-df = pd.read_csv('D:\ML-5\data\Cleaned_data.csv')
-
-# Drop columns with names: min_price, max_price
-
-df.drop(['min_price', 'max_price'], axis=1, inplace=True)
-
-print(df.head())
-
-## Now we train a model to predict the modal_price based on the arrivals_in_qtl, APMC, Commodity, and date
-## This is clearly a regression task
-
 # Import libraries
+import pandas as pd
+import pickle
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
 
-# Since we have categorical features we need a label encoder to convert them to numerical values
-le = LabelEncoder()
-df['APMC'] = le.fit_transform(df['APMC'])
-df['Commodity'] = le.fit_transform(df['Commodity'])
+# Load cleaned data
+df = pd.read_csv('D:\ML-5\data\Cleaned_data.csv')
 
+# Drop columns with names: min_price, max_price
+df.drop(['min_price', 'max_price'], axis=1, inplace=True)
+print(df.head())
+
+# Now we train a model to predict the modal_price based 
+# on the arrivals_in_qtl, APMC, Commodity, and date
+# Since we have categorical features we need a label encoder
+# to convert them to numerical values
+le1 = LabelEncoder()
+df['APMC'] = le1.fit_transform(df['APMC'])
+le2= LabelEncoder()
+df['Commodity'] = le2.fit_transform(df['Commodity'])
 
 # We will need to convert the date column to datetime format
 df['date'] = pd.to_datetime(df['date'])
 df['date'] = df['date'].dt.strftime('%Y%m').astype(int)
-
+print(df.head())
 # Split the data into train and test sets
 X = df.drop(['modal_price'], axis=1)
 y = df['modal_price']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Create a random forest regressor object
-
+# This is clearly a regression task. We create a random forest regressor object.
 rf = RandomForestRegressor(n_estimators=100, max_depth=10, min_samples_leaf=2, min_samples_split=2)
 
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-
 # We will do ensemble learning of polynomial regressor and Random Forest Regresson using stacking
-
 model = make_pipeline(PolynomialFeatures(2), rf)
-
 model.fit(X_train, y_train)
 
 # Predict on the test set
-
 y_pred = model.predict(X_test)
 
 # Calculate the mean absolute error
-
 mae = mean_absolute_error(y_test, y_pred)
-
 print('Mean absolute error: ', mae)
 
 
 # We save the model and label encoder using pickle
-
-import pickle
-
-pickle.dump(model, open('D:\ML-5\saved\model.pkl', 'wb'))
-
-pickle.dump(le, open('D:\ML-5\saved\label_encoder.pkl', 'wb'))
-
-
-
-
-
+# pickle.dump(model, open('D:\ML-5\saved\model.pkl', 'wb'))
+# pickle.dump(le1, open('D:\ML-5\saved\label_encoder_apmc.pkl', 'wb'))
+# pickle.dump(le2, open('D:\ML-5\saved\label_encoder_commodity.pkl', 'wb'))
+print('Model and label encoders saved successfully')
